55a244e74c138ec3a8069fb6fbfeee015e7cec90
Andreas Roth <aroth@arsoft-online.com>
2016-02-19 07:26:47 +0100

    initial commit

---
+++ debian/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = array();
+
+/* Default addressbook source */
+$config['default_addressbook'] = '0';
+
+/* database table name */
+$config['db_table_google_contacts'] = 'google_contacts';
+
+/* max results */
+$config['google_contacts_max_results'] = 1000;
+
+?>
